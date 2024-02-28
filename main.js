efe639c0ca223a095b34c57eb2e65e9b3bf166b0
(haider ali) <(haideralikarachi8@gmail.com)>
2024-02-28 16:12:07 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+// Assignment # 1
+// Store a person's name in a variables
+var PersonName = "Haider Ali";
+// Print a message to the person
+console.log("Hello ".concat(PersonName, " What is your is your qualification?"));
---
+++ main.ts
@@ -0,0 +1,10 @@
+ // Assignment # 1
+
+ // Store a person's name in a variables
+
+ var PersonName : string = "Haider Ali"
+
+ // Print a message to the person
+
+ console.log(`Hello ${PersonName} What is your is your qualification?`)
+
